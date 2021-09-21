d76e16ebae32bca2ffa149b7caeaa9aa3ddf6b69
NicolasXM <90805926+NicolasXM@users.noreply.github.com>
2021-09-21 06:25:28 -0300

    Atualização caso de teste e mockito
    Atualização caso de teste e mockito

--- lib/modules/search/domain/repositories/teste_repository.dart
+++ lib/modules/search/domain/repositories/teste_repository.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:inspec_solar/modules/search/domain/entities/teste_search.dart';
 import 'package:inspec_solar/modules/search/domain/errors/error_search.dart';
-import 'package:inspec_solar/modules/search/domain/usecases/search_text.dart';
 
 abstract class TesteRepository {
   Future<Either<ErrorSearch, List<TesteSearch>>> search(String listsearch);
--- test/moduloes/search/domain/usecases/search_text_test.dart
+++ test/moduloes/search/domain/usecases/search_text_test.dart
@@ -4,20 +4,19 @@ import 'package:inspec_solar/modules/search/domain/entities/teste_search.dart';
 import 'package:inspec_solar/modules/search/domain/errors/error_search.dart';
 import 'package:inspec_solar/modules/search/domain/repositories/teste_repository.dart';
 import 'package:inspec_solar/modules/search/domain/usecases/search_text.dart';
+import 'package:mockito/mockito.dart';
 
-class TesteRepositoryImp implements TesteRepository {
-  @override
-  Future<Either<ErrorSearch, List<TesteSearch>>> search(String listsearch) {
-    throw UnimplementedError();
-  }
-}
+class TesteRepositoryImp extends Mock implements TesteRepository {}
 
 main() {
   final repository = TesteRepositoryImp();
   final usecase = SearchByTextImpl(repository);
 
   test('teste de retorno', () async {
-    final result = await usecase("teste");
+    when(
+        repository.search(any).thenAnswer((_) async => Right(<TesteSearch>[])));
+
+    late final result = usecase("teste");
 
     expect(result | null, isA<List<TesteSearch>>());
   });
